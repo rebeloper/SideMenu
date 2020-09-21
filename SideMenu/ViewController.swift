0d818c33f6dd0c64b5cf30f1a94d80d398c94b9b
Alex Nagy <therebeloper@gmail.com>
2020-09-21 12:29:37 +0300

    Initial Commit

---
+++ SideMenu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SideMenu
+//
+//  Created by Alex Nagy on 21/09/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
